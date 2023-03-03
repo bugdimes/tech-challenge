42e30bd4cf7e742f08da4f91737a040770144b6d
Divyesh Patel <pateldivyesh009@gmail.com>
2023-03-02 22:49:14 -0800

    comments added for one-liners

--- array_hashes.rb
+++ array_hashes.rb
@@ -17,14 +17,14 @@ x = [{"a" => 10},{"b" => 20},{"c" => 30}]
 # Note: There are many ways to operate, I am going with One-liners
 
 # 1. one array containing all keys
-p x.flat_map(&:keys)
-p x.collect { |p| p.keys }.flatten
+p x.flat_map(&:keys) # solution 1
+p x.collect { |p| p.keys }.flatten # solution 2
 
 # 2. another containing all values
-p x.collect { |p| p.values }.flatten
-p x.flat_map(&:values)
+p x.collect { |p| p.values }.flatten # solution 1
+p x.flat_map(&:values) # solution 2
 
 # 3. the sum of all the values
-p x.flat_map(&:values).sum
-p x.collect { |p| p.values }.flatten.sum
+p x.flat_map(&:values).sum # solution 1
+p x.collect { |p| p.values }.flatten.sum # solution 2
 
